17e07c7cc620c3c2f4ddfe77f7aecae104eba252
She <algoprazolam@gmail.com>
2023-10-17 19:06:56 +0200

    adding dist subtree

---
+++ dist/index.c9c7fc2d.js
@@ -0,0 +1,25 @@
+const app = Vue.createApp({
+    data () {
+        return {
+            mainNavItems: [
+                {
+                    text: "Vue item 1"
+                },
+                {
+                    text: "Vue item 2"
+                },
+                {
+                    text: "Vue item 3"
+                },
+                {
+                    text: "Vue item 4"
+                },
+                {
+                    text: "Vue item 5"
+                }
+            ]
+        };
+    }
+});
+
+//# sourceMappingURL=index.c9c7fc2d.js.map
