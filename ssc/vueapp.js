41e53d7aa99d9548baef3a5fe2bd2ec1fe0f9972
She <algoprazolam@gmail.com>
2023-10-17 18:59:49 +0200

    vue should work now, had to install vue into parcel

--- ssc/vueapp.js
+++ ssc/vueapp.js
@@ -1,13 +1,12 @@
 const app = Vue.createApp({
-    let id = 0;
     data() {
         return {
             mainNavItems: [
-                {id: id++, text: 'Vue item 1'},
-                {id: id++, text: 'Vue item 2'},
-                {id: id++, text: 'Vue item 3'},
-                {id: id++, text: 'Vue item 4'},
-                {id: id++, text: 'Vue item 5'}
+                {text: 'Vue item 1'},
+                {text: 'Vue item 2'},
+                {text: 'Vue item 3'},
+                {text: 'Vue item 4'},
+                {text: 'Vue item 5'}
             ]
         }
         
