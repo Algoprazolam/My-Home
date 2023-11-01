4fded3b56d5bd072be1417677c1217c04eaa4c0f
She <algoprazolam@gmail.com>
2023-11-01 17:45:38 +0100

    started formatting the article

--- src/components/typewriter.vue
+++ src/components/typewriter.vue
@@ -12,19 +12,20 @@
     
     <hr>
     
-    <p>
-    **Algoprazolam** (/ˈ<u>alɡəʊpræzəlæm</u>/), that's just me.
-    
-    </p>
     <article id="wikipediaBox">
         <img src="../assets/images/algoMini.jpg" alt="">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, cumque!</p>
     </article>
+    <p>
+        <span class="bold">Algoprazolam</span> (/ˈ<u>alɡəʊpræzəlæm</u>/), that's just me.
+    
+    </p>
+    
     <h2>Algos</h2>
     <hr>
 
 <p>
-    **Algos** in Greek is a neuter noun literally meaning "pain". The name is related to the word suffix '-algia' denoting a painful condition.<sup>[3]</sup> 
+    <span class="bold">Algos</span> in Greek is a neuter noun literally meaning "pain". The name is related to the word suffix '-algia' denoting a painful condition.<sup>[3]</sup> 
 </p>
 <h2>Algo</h2>
     <hr>
@@ -44,12 +45,18 @@
 
 <style scoped>
 
+#wikipediaArticle {
+    color: white;
+}
+.bold {
+    font-weight: bold;
+    font-size: calc(1.2rem + 5px);
+}
 #wikipediaBox {
     float: right;
     border: 1px solid white;
-    width: 40%;
+    width: 271px;
     height: auto;
-    max-width: 250px;
 }
 
 #wikipediaBox img {
@@ -58,15 +65,14 @@
 hr {
     margin-right: 16px;
 }
-h1, h2, p{
-    color: white;
-}
 h1 {
+    padding-top: 40px;
     font-size: calc(2rem + 0.5vw);
     font-weight: bold;
 }
 h1 + hr {
     border-top: 3px white solid;
+    margin-bottom: 50px;
 }
 h2 {
     font-size: 1.8rem;
